af647b54c50c58595e9a3f7c280f941f85f31866
andreyshabalin <andrey.shabalin@gmail.com>
2016-04-19 01:19:32 -0400

    removed "ramwas:::"
    ' -> "

--- R/ramwas.r
+++ R/ramwas.r
@@ -3,7 +3,7 @@
 library(Rsamtools)
 ### GenomicAlignments to process CIGAR strings
 library(GenomicAlignments)
-library(snow)
+#library(snow)
 
 .ramwasEnv = new.env()
 
@@ -28,13 +28,13 @@ library(snow)
 	return(FALSE);
 }
 if(FALSE) {
-	.isAbsolutePath( 'C:/123' );  # TRUE
-	.isAbsolutePath( '~123' );    # FALSE
-	.isAbsolutePath( '~/123' );   # TRUE
-	.isAbsolutePath( '/123' );    # TRUE
-	.isAbsolutePath( '\\123' );    # TRUE
-	.isAbsolutePath( 'asd\\123' ); # FALSE
-	.isAbsolutePath( 'a\\123' );   # FALSE
+	.isAbsolutePath( "C:/123" );  # TRUE
+	.isAbsolutePath( "~123" );    # FALSE
+	.isAbsolutePath( "~/123" );   # TRUE
+	.isAbsolutePath( "/123" );    # TRUE
+	.isAbsolutePath( "\\123" );    # TRUE
+	.isAbsolutePath( "asd\\123" ); # FALSE
+	.isAbsolutePath( "a\\123" );   # FALSE
 	
 }
 
@@ -43,7 +43,7 @@ if(FALSE) {
 parametersFromFile = function(.parameterfile){
 	source(.parameterfile, local = TRUE);
 	.nms = ls();
-	# .result = vector('list',length(.nms));
+	# .result = vector("list",length(.nms));
 	# names(.result) = .nms;
 	# for( .i in seq_along(.nms)) {
 	# 	.result[[.i]] = get(.nms[.i]);
@@ -52,7 +52,7 @@ parametersFromFile = function(.parameterfile){
 	return(mget(.nms));
 }
 if(FALSE) { # test code
-	param = parametersFromFile(.parameterfile = 'D:/RW/NESDA/ramwas/param_file.txt');
+	param = parametersFromFile(.parameterfile = "D:/RW/NESDA/ramwas/param_file.txt");
 	param
 }
 
@@ -69,9 +69,9 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 	### More than 500 alignments per read is unlikely (although very possible)
 	max.alignments.per.read = 500; 
 	
-	fields = c('qname','rname','pos','cigar','flag') 		
-	# 'qname' is read name, 'rname' is chromosome
-	tags = 'NM';# character();
+	fields = c("qname","rname","pos","cigar","flag") 		
+	# "qname" is read name, "rname" is chromosome
+	tags = "NM";# character();
 	
 	### Open the BAM file
 	{
@@ -104,25 +104,25 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 	# oldtail = NULL; 
 	repeat{
 		### break condition
-		### is at the begining of the loop to support calls of 'next'
+		### is at the begining of the loop to support calls of "next"
 		# if(is.logical(oldtail))
 		# 	break;
 		
-		### Read 'yieldSize' rows
+		### Read "yieldSize" rows
 		bb = scanBam(file=bf, param=param)[[1]];
 		if( length(bb[[1]])==0 )
 			break;
 		
 		### Put tags in the main list
-		bb = c(bb[names(bb) != 'tag'], bb$tag);
+		bb = c(bb[names(bb) != "tag"], bb$tag);
 		# data.frame(lapply(bb,`[`, 1:60), check.rows = FALSE, stringsAsFactors = FALSE)
 
 		stopifnot( length(bb[[scoretag]]) == length(bb[[1]]) )
 		
 		### Create output lists
 		if(is.null(startlistfwd)) {
-			startlistfwd = vector('list',length(levels(bb$rname)));
-			startlistrev = vector('list',length(levels(bb$rname)));
+			startlistfwd = vector("list",length(levels(bb$rname)));
+			startlistrev = vector("list",length(levels(bb$rname)));
 			names(startlistfwd) = levels(bb$rname);
 			names(startlistrev) = levels(bb$rname);
 			startlistfwd[] = list(list())
@@ -134,7 +134,7 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 		# 	bbsize = length(bb[[1]]);
 		# 	if( bbsize == 0L ) {
 		# 		if(is.null(oldtail))
-		# 			stop('Empty BAM file (?).');
+		# 			stop("Empty BAM file (?).");
 		# 		bb = oldtail;
 		# 		oldtail = FALSE;
 		# 	} else {
@@ -200,8 +200,8 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 		{
 			offset = length(startlistfwd);
 			split.levels = as.integer(bb$rname) + offset*bb$isReverse;
-			levels(split.levels) = c(names(startlistfwd),paste0(names(startlistfwd),'-'));
-			class(split.levels) = 'factor';
+			levels(split.levels) = c(names(startlistfwd),paste0(names(startlistfwd),"-"));
+			class(split.levels) = "factor";
 			splt = split( bb$startpos, split.levels, drop = FALSE);
 			# print(sapply(splt,length))
 			for( i in seq_along(startlistfwd) ) {
@@ -214,7 +214,7 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 			}
 			rm(offset, split.levels, splt);
 		} # startlistfwd, startlistrev	
-		cat(sprintf('Recorded %.f of %.f reads',qc$reads.recorded,qc$reads.total),'\n')
+		cat(sprintf("Recorded %.f of %.f reads",qc$reads.recorded,qc$reads.total),"\n")
 	}
 	close(bf);
 	rm(bf); # , oldtail
@@ -222,7 +222,7 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 	startsfwd = startlistfwd;
 	startsrev = startlistrev;
 	
-	### combine and sort lists in 'outlist'
+	### combine and sort lists in "outlist"
 	for( i in seq_along(startlistfwd) ) {
 		startsfwd[[i]] = sort.int(unlist(startlistfwd[[i]]));
 		startsrev[[i]] = sort.int(unlist(startlistrev[[i]]));
@@ -232,9 +232,9 @@ bam.scanBamFile = function( bamfilename, scoretag = "mapq", minscore = 4){
 	return( bam );
 }
 if(FALSE) { # test code
-	bamfilename = 'D:/02H08SM142EZ.bam'; scoretag = "AS"; minscore = 10;
-	rbam = bam.scanBamFile(bamfilename = 'D:/02H08SM142EZ.bam', scoretag = "AS", minscore = 10);
-	sprintf('Recorded %.f of %.f reads',1e4,1e10)
+	bamfilename = "D:/02H08SM142EZ.bam"; scoretag = "AS"; minscore = 10;
+	rbam = bam.scanBamFile(bamfilename = "D:/02H08SM142EZ.bam", scoretag = "AS", minscore = 10);
+	sprintf("Recorded %.f of %.f reads",1e4,1e10)
 }
 ###
 ### BAM QC / preprocessing
@@ -276,7 +276,7 @@ bam.removeRepeats = function(rbam, maxrep){
 
 ### Non-CpG set of locations
 noncpgSitesFromCpGset = function(cpgset, distance){
-	noncpg = vector('list', length(cpgset));
+	noncpg = vector("list", length(cpgset));
 	names(noncpg) = names(cpgset);
 	for( i in seq_along(cpgset) ) { # i=1;
 		pos = cpgset[[i]];
@@ -289,7 +289,7 @@ noncpgSitesFromCpGset = function(cpgset, distance){
 }
 ### Find isolated CpGs among the given set of CpGs
 isocpgSitesFromCpGset = function(cpgset, distance){
-	isocpg = vector('list',length(cpgset));
+	isocpg = vector("list",length(cpgset));
 	names(isocpg) = names(cpgset);
 	for( i in seq_along(cpgset) ) {	
 		distbig = diff(cpgset[[i]]) >= distance;
@@ -298,7 +298,7 @@ isocpgSitesFromCpGset = function(cpgset, distance){
 	return(isocpg);
 }
 if(FALSE) { # test code
-	cpgset = readRDS('C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19/spgset_hg19_SNPS_at_MAF_0.05.rds')
+	cpgset = readRDS("C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19/spgset_hg19_SNPS_at_MAF_0.05.rds")
 	noncpg = noncpgSitesFromCpGset(cpgset, 200);
 	sapply(cpgset, typeof)
 	sapply(noncpg, typeof)
@@ -307,7 +307,7 @@ if(FALSE) { # test code
 	
 	
 	cpgset = lapply(1:10, function(x){return(c(1,1+x,1+2*x))})
-	names(cpgset) = paste0('chr',seq_along(cpgset))
+	names(cpgset) = paste0("chr",seq_along(cpgset))
 	show(cpgset);
 	noncpg = noncpgSitesFromCpGset(cpgset, 3);
 	show(noncpg);
@@ -366,7 +366,7 @@ if(FALSE) { # test code
 	system.time( .count.nonCpG.reads.forward2(starts, cpglocations, distance=20) )
 	
 	rbam2 = bam.count.nonCpG.reads(rbam, toycpgset, 50)
-	cat( rbam2$qc$bam.count.nonCpG.reads[1], 'of',rbam2$qc$bam.count.nonCpG.reads[2],'reads are not covering CpGs','\n' );
+	cat( rbam2$qc$bam.count.nonCpG.reads[1], "of",rbam2$qc$bam.count.nonCpG.reads[2],"reads are not covering CpGs","\n" );
 }
 
 ### Get distribution of distances to isolated CpGs
@@ -447,7 +447,7 @@ estimateFragmentSizeDistribution = function(hist.isolated.distances, seqLength){
 	x = seq_along(hist.isolated.distances);
 
 	# plot( x, hist.isolated.distances)
-	# lines( x, fsPredict(x, initparam), col='blue', lwd = 3)
+	# lines( x, fsPredict(x, initparam), col="blue", lwd = 3)
 
 	fmin = function(param) {
 		fit2 = fsPredict(x, param); 
@@ -470,7 +470,7 @@ estimateFragmentSizeDistribution = function(hist.isolated.distances, seqLength){
 	rezfit = rezfit / rezfit[1];
 	
 	# lz = lm(hist.isolated.distances[seq_along(rezfit)] ~ rezfit)
-	# lines(rezfit*lz$coefficients[2]+lz$coefficients[1], lwd = 4, col='red');
+	# lines(rezfit*lz$coefficients[2]+lz$coefficients[1], lwd = 4, col="red");
 	
 	return(rezfit);
 }
@@ -480,17 +480,17 @@ if(FALSE) { # test code
 	plot(x,y)
 	log.ss <- nls(y ~ SSlogis(x, phi1, phi2, phi3))
 	z = SSlogis(x, 0.59699, 0.61320, 0.04599)
-	lines(x, z, col='blue')
+	lines(x, z, col="blue")
 	
 	
-	# setwd('D:/RW/NESDA/ramwas/AS120_sba/');
-	setwd('D:/RW/RC2/ramwas/AS38_gap0/');
-	# setwd('D:/RW/Celltype//ramwas/AS120_sba/');
-	lst = list.files(pattern = '\\.qc\\.');
+	# setwd("D:/RW/NESDA/ramwas/AS120_sba/");
+	setwd("D:/RW/RC2/ramwas/AS38_gap0/");
+	# setwd("D:/RW/Celltype//ramwas/AS120_sba/");
+	lst = list.files(pattern = "\\.qc\\.");
 	qcs = lapply(lst, function(x){load(x);return(bam);})
-	histinfo = Reduce( `+`, lapply( lapply(qcs, `[[`, 'qcflt'), `[[`, 'hist.iso.dist.250'), init = 0);
+	histinfo = Reduce( `+`, lapply( lapply(qcs, `[[`, "qcflt"), `[[`, "hist.iso.dist.250"), init = 0);
 	rng = range(histinfo[-(1:10)]);
-	plot(histinfo/1e3, ylim = rng/1e3, pch=19, col='blue')
+	plot(histinfo/1e3, ylim = rng/1e3, pch=19, col="blue")
 	
 	hist.isolated.distances = histinfo;
 	seqLength = 50;
@@ -500,20 +500,20 @@ if(FALSE) { # test code
 	x = seq_along(hist.isolated.distances)
 	plot( x, hist.isolated.distances)
 	lz = lm(hist.isolated.distances[seq_along(fit)] ~ fit)
-	lines(fit*lz$coefficients[2]+lz$coefficients[1], lwd = 4, col='red');
+	lines(fit*lz$coefficients[2]+lz$coefficients[1], lwd = 4, col="red");
 	
 }
 
 ### Cache CpG location files to avoid reloading.
 cachedRDSload = function(rdsfilename){
-	globalname = rdsfilename; #paste0('.ramwas.',rdsfilename);
-	if( exists(x = globalname, envir = ramwas:::.ramwasEnv) ) {
-		cat('Using cache','\n');
-		return(base::mget(x = globalname, envir = ramwas:::.ramwasEnv));
+	globalname = rdsfilename; #paste0(".ramwas.",rdsfilename);
+	if( exists(x = globalname, envir = .ramwasEnv) ) {
+		cat("Using cache","\n");
+		return(base::mget(x = globalname, envir = .ramwasEnv));
 	} else {
-		cat('Loading','\n');
+		cat("Loading","\n");
 		data = readRDS(rdsfilename);
-		base::assign(x = globalname, value = data, envir = ramwas:::.ramwasEnv);
+		base::assign(x = globalname, value = data, envir = .ramwasEnv);
 		return(data);
 	}
 }
@@ -521,16 +521,8 @@ if(FALSE) { # test code
 	rdsfilename = "C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19//spgset_hg19_SNPS_at_MAF_0.05.rds";
 	system.time({z = cachedRDSload(rdsfilename)});
 	system.time({z = cachedRDSload(rdsfilename)});
-	system.time({z = cachedRDSload(rdsfilename)});.
+	system.time({z = cachedRDSload(rdsfilename)});
 	
-	globalname = paste0('.ramwas.',rdsfilename);
-	if( exists(x = globalname, envir = ramwas:::.ramwasEnv) ) {
-		return(base::mget(x = globalname, envir = ramwas:::.ramwasEnv));
-	} else {
-		data = readRDS(rdsfilename);
-		base::assign(x = globalname, value = data, envir = ramwas:::.ramwasEnv);
-		return(data);
-	}
 }
 
 ### Pipeline parts
@@ -543,27 +535,27 @@ pipelineProcessBam = function(bamname, param) {
 	if( is.null(param$maxrepeats) )
 		param$maxrepeats = 0;
 	if( !is.null(param$cpgfile) && is.null(param$maxfragmentsize) )
-		return('Parameter not set: maxfragmentsize');
+		return("Parameter not set: maxfragmentsize");
 	
-	bamname = gsub('\\.bam$','',bamname);
+	bamname = gsub("\\.bam$","",bamname);
 	if( !.isAbsolutePath(bamname) && (length(param$bamdir)>0) ) {
-		bamfullname = paste0(param$bamdir, '/', bamname, '.bam');
+		bamfullname = paste0(param$bamdir, "/", bamname, ".bam");
 	} else {
-		bamfullname = paste0(bamname, '.bam');
+		bamfullname = paste0(bamname, ".bam");
 	}
 	
-	rdsbmdir = paste0( param$projectdir, "/", param$scoretag, "_", param$minscore, '/rbam_rds');
-	rdsqcdir = paste0( param$projectdir, "/", param$scoretag, "_", param$minscore, '/rbam_qc_rds');
+	rdsbmdir = paste0( param$projectdir, "/", param$scoretag, "_", param$minscore, "/rbam_rds");
+	rdsqcdir = paste0( param$projectdir, "/", param$scoretag, "_", param$minscore, "/rbam_qc_rds");
 	dir.create(rdsbmdir, showWarnings = FALSE, recursive = TRUE)
 	dir.create(rdsqcdir, showWarnings = FALSE, recursive = TRUE)
 	
-	rdsbmfile = paste0( rdsbmdir, '/', basename(bamname), '.rbam.rds' );
-	rdsqcfile = paste0( rdsqcdir, '/', basename(bamname), '.qc.rds' );
+	rdsbmfile = paste0( rdsbmdir, "/", basename(bamname), ".rbam.rds" );
+	rdsqcfile = paste0( rdsqcdir, "/", basename(bamname), ".qc.rds" );
 	if( file.exists( rdsqcfile ) )
-		return(paste0('Rbam qc rds file already exists: ',rdsqcfile));
+		return(paste0("Rbam qc rds file already exists: ",rdsqcfile));
 	
 	if( !file.exists( bamfullname ) )
-		return(paste0('Bam file does not exist: ',bamfullname));
+		return(paste0("Bam file does not exist: ",bamfullname));
 	
 	rbam = bam.scanBamFile(bamfilename = bamfullname, scoretag = param$scoretag, minscore = param$minscore);
 	
@@ -584,12 +576,12 @@ pipelineProcessBam = function(bamname, param) {
 		rbam4 = rbam2;
 	}
 	
-	saveRDS( object = rbam4, file = rdsbmfile, compress = 'xz');
+	saveRDS( object = rbam4, file = rdsbmfile, compress = "xz");
 	rbam5 = rbam4;
 	rbam5$startsfwd=NULL;
 	rbam5$startsrev=NULL;
-	saveRDS( object = rbam5, file = rdsqcfile, compress = 'xz');
-	return(paste0('OK. ', bamname));
+	saveRDS( object = rbam5, file = rdsqcfile, compress = "xz");
+	return(paste0("OK. ", bamname));
 }
 
 ### RaMWAS pipeline
@@ -607,7 +599,7 @@ ramwas1scanBams = function( param ){
 		cl <- makeCluster(param$cputhreads)
 		# clusterExport(cl, list = c("nms", "rvcfdir"))
 		# nmslist = clusterSplit(cl, nms)
-		# z = clusterApplyLB(cl, 1:8, function(i){ vcf = readRDS(paste0(rvcfdir,'/Rvcf_',nms[i],'.rds')); return(vcf$pos)})
+		# z = clusterApplyLB(cl, 1:8, function(i){ vcf = readRDS(paste0(rvcfdir,"/Rvcf_",nms[i],".rds")); return(vcf$pos)})
 		z = clusterApplyLB(cl, bamnames, pipelineProcessBam, param=param)
 		stopCluster(cl)
 	}
@@ -615,13 +607,13 @@ ramwas1scanBams = function( param ){
 }
 if(FALSE) { # test code
 	param = list(
-		bamdir = 'C:/Cell_type/bams/',
-		projectdir = 'D:/Cell_type/',
-		bamlistfile = 'c:/Cell_type/000_list_of_files.txt',
+		bamdir = "C:/Cell_type/bams/",
+		projectdir = "D:/Cell_type/",
+		bamlistfile = "C:/Cell_type/000_list_of_files.txt",
 		scoretag = "AS",
 		minscore = 100,
 		cputhreads = 6,
-		cpgfile = 'C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19/spgset_hg19_SNPS_at_MAF_0.05.rds',
+		cpgfile = "C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19/spgset_hg19_SNPS_at_MAF_0.05.rds",
 		noncpgfile = NULL,
 		maxrepeats = 3,
 		maxfragmentsize=200,
@@ -629,13 +621,13 @@ if(FALSE) { # test code
 	);
 	
 	param = list(
-		bamdir = '.',
-		projectdir = 'D:/Cell_type/',
-		bamlistfile = 'c:/Cell_type/000_list_of_files.txt',
+		bamdir = ".",
+		projectdir = "D:/Cell_type/",
+		bamlistfile = "c:/Cell_type/000_list_of_files.txt",
 		scoretag = "AS",
 		minscore = 100,
 		cputhreads = 8,
-		cpgfile = 'C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19/spgset_hg19_SNPS_at_MAF_0.05.rds',
+		cpgfile = "C:/AllWorkFiles/Andrey/VCU/RaMWAS_2/code/Prepare_CpG_list/hg19/spgset_hg19_SNPS_at_MAF_0.05.rds",
 		noncpgfile = NULL,
 		maxrepeats = 3,
 		maxfragmentsize=200,
@@ -645,7 +637,7 @@ if(FALSE) { # test code
 	library(ramwas)
 	# ramwas1scanBams(param)
 	
-	bamname='150114_WBCS014_CD20_150.bam';
+	bamname="150114_WBCS014_CD20_150.bam";
 	{
 		tic = proc.time();
 		pipelineProcessBam(bamname, param);
@@ -654,23 +646,23 @@ if(FALSE) { # test code
 	}
 	
 	bamnames = readLines(param$bamlistfile);
-	bamnames[seq(1, length(bamnames), 2)] = paste0('C:/Cell_type/bams/', bamnames[seq(1, length(bamnames), 2)]);
-	bamnames[seq(2, length(bamnames), 2)] = paste0('D:/Cell_type/bams/', bamnames[seq(2, length(bamnames), 2)]);
+	bamnames[seq(1, length(bamnames), 2)] = paste0("C:/Cell_type/bams/", bamnames[seq(1, length(bamnames), 2)]);
+	bamnames[seq(2, length(bamnames), 2)] = paste0("D:/Cell_type/bams/", bamnames[seq(2, length(bamnames), 2)]);
 	
-	library(snow)
-	library(ramwas)
+	# library(snow)
+	# library(ramwas)
 	# cl = makeCluster(param$cputhreads)
 	cl = makeSOCKcluster(rep("localhost",param$cputhreads))
-	# clusterCall(cl, function(){library('ramwas')});
+	# clusterCall(cl, function(){library("ramwas")});
 	clusterEvalQ(cl, library(ramwas))
 	
 	# nmslist = clusterSplit(cl, nms)
-	# z = clusterApplyLB(cl, 1:8, function(i){ vcf = readRDS(paste0(rvcfdir,'/Rvcf_',nms[i],'.rds')); return(vcf$pos)})
+	# z = clusterApplyLB(cl, 1:8, function(i){ vcf = readRDS(paste0(rvcfdir,"/Rvcf_",nms[i],".rds")); return(vcf$pos)})
 	report = clusterApplyLB(cl, bamnames, pipelineProcessBam, param=param)
 	stopCluster(cl)
 	
-	file.exists('D:/Cell_type/bams/141106_WBCS011_BuCo_350.bam')
-	file.exists('D:/Cell_type/bams//141201_WBCS011_BuCo_150.bam')
+	file.exists("D:/Cell_type/bams/141106_WBCS011_BuCo_350.bam")
+	file.exists("D:/Cell_type/bams//141201_WBCS011_BuCo_150.bam")
 	
 	### Cell type performance:
 	# 15:35 - 17:40
