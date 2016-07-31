6269fee89ffefc7b3711f84e7f699e226b4dbfb7
andreyshabalin <andrey.shabalin@gmail.com>
2016-07-31 16:13:54 -0400

    Added C method registration. Moved C code in one file.

--- src/rw_c_code.c
+++ src/rw_c_code.c
@@ -1,6 +1,45 @@
 #include <R.h>
 #include <Rinternals.h>
+#include <R_ext/Rdynload.h>
 
+SEXP CrowSumsSq(SEXP input, SEXP outpt)
+{
+	input = PROTECT(coerceVector(input, REALSXP));
+	outpt = PROTECT(coerceVector(outpt, REALSXP));
+	int nrow = length(outpt);
+	int ncol = length(input) / nrow;
+	
+	double *xinput = REAL(input);
+	double *xoutpt = REAL(outpt);
+	
+	for(int j = 0; j < ncol; j++) 
+		for(int i = 0; i < nrow; i++)
+			xoutpt[i] += xinput[j*nrow+i] * xinput[j*nrow+i];
+	UNPROTECT(2);
+	return R_NilValue;
+}
+
+SEXP CcolSumsSq(SEXP input, SEXP outpt)
+{
+	input = PROTECT(coerceVector(input, REALSXP));
+	outpt = PROTECT(coerceVector(outpt, REALSXP));
+	int ncol = length(outpt);
+	int nrow = length(input) / ncol;
+	
+	double *xinput = REAL(input);
+	double *xoutpt = REAL(outpt);
+	
+	for(int j = 0; j < ncol; j++) {
+		double sm = 0;
+		for(int i = 0; i < nrow; i++)
+			sm += xinput[j*nrow+i] * xinput[j*nrow+i];
+		xoutpt[j] = sm;
+	}
+	UNPROTECT(2);
+	return R_NilValue;
+}
+
+	
 SEXP cover_frw_c(SEXP starts, SEXP cpgs, SEXP fragdist, SEXP ind1, SEXP ind2, SEXP out) {
 	starts   = PROTECT(coerceVector(starts,    INTSXP));
 	cpgs     = PROTECT(coerceVector(cpgs,      INTSXP));
@@ -63,4 +102,16 @@ SEXP cover_rev_c(SEXP starts, SEXP cpgs, SEXP fragdist, SEXP ind1, SEXP ind2, SE
 	
 	UNPROTECT(6);
 	return(R_NilValue);
+}
+
+static R_CallMethodDef callMethods[] = {
+	{"CrowSumsSq", (DL_FUNC) &CrowSumsSq, 2},
+	{"CcolSumsSq", (DL_FUNC) &CcolSumsSq, 2},
+	{"cover_frw_c", (DL_FUNC) &cover_frw_c, 6},
+	{"cover_rev_c", (DL_FUNC) &cover_rev_c, 6},
+	{NULL, NULL, 0}
+};
+
+void R_init_myLib(DllInfo *info)	{
+	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
 }
