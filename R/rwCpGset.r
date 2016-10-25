ed6f7e8b0aafbf2e8d1611dc5f451135a2565cd0
andreyshabalin <andrey.shabalin@gmail.com>
2016-10-25 01:39:25 -0400

    Added injectSNPsMAF() function.

--- R/rwCpGset.r
+++ R/rwCpGset.r
@@ -94,4 +94,71 @@ insilicoFASTQ = function(con, gensequence, fraglength){
 	return(invisible(TRUE));
 }
 
+injectSNPsMAF = function(gensequence, frqcount, MAF = 0.01){ # 
+	# http://droog.gs.washington.edu/parc/images/iupac.html
+	{
+		strACGT = array('', c(2,2,2,2));
+		
+		strACGT[1,1,1,1] = '*';
+		
+		strACGT[2,1,1,1] = 'A';
+		strACGT[1,2,1,1] = 'C';
+		strACGT[1,1,2,1] = 'G';
+		strACGT[1,1,1,2] = 'T';
+		
+		strACGT[2,2,1,1] = 'M';
+		strACGT[2,1,2,1] = 'R';
+		strACGT[2,1,1,2] = 'W';
+		strACGT[1,2,2,1] = 'S';
+		strACGT[1,2,1,2] = 'Y';
+		strACGT[1,1,2,2] = 'K';
+		
+		strACGT[2,2,2,1] = 'V';
+		strACGT[2,2,1,2] = 'H';
+		strACGT[2,1,2,2] = 'D';
+		strACGT[1,2,2,2] = 'B';
+		
+		strACGT[2,2,2,2] = 'N';
+		
+		rawACGT = sapply(strACGT, charToRaw)
+		dim(rawACGT) = dim(strACGT)
+		
+		ACGT = 1:4
+		ACGTnames = c('A','C','G','T')
+		names(ACGT) = ACGTnames
+	}
+	# chrom = charToRaw( readRDS(filechr) );
+	if( length(frqcount)==1 ) {
+		frqcount = readLines(con = frqcount);
+	}
+	if(grepl('^CHROM\t',frqcount[1]))
+		frqcount = frqcount[-1];
+	
+	spt = strsplit(x = frqcount, split = '\t', fixed = TRUE);
+	rm(frqcount);
+	gc();
+	
+	pos = as.integer( sapply( spt, `[`, 2) );
 
+	gensequence = charToRaw(as.character(gensequence));
+	
+	# options(warn=2)
+	for( i in seq_along(spt)){ # i=17
+		if( (i %% 10000) == 0 )
+			message('Step ', i, ' of', length(spt));
+		tl = spt[[i]][-(1:4)]
+		tlspt = strsplit(tl,':',TRUE);
+		allele = sapply(tlspt,`[`,1);
+		count = as.integer(sapply(tlspt,tail,1));
+		names(count) = allele;
+		countACGT = count[ACGTnames];
+		countACGT[is.na(countACGT)] = 0;
+		if(sum(countACGT) > 0 ) {
+			countACGT = countACGT / sum(countACGT);
+			index = (countACGT >= MAF)+1L;
+			dim(index) = c(1,4);
+			gensequence[pos[i]] = rawACGT[index];
+		}
+	}
+	return(rawToChar(gensequence));
+}
