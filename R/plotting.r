afa584a4d9c7db4d78bd62482f65267f86ee458b
andreyshabalin <andrey.shabalin@gmail.com>
2017-12-08 20:53:05 -0700

    Add Manhattan plot functions and manual.

--- R/plotting.r
+++ R/plotting.r
@@ -144,68 +144,122 @@ qqPlotFast = function(
     return(invisible(qq));
 }
 
+manPlotPrepare = function(
+            pvalues,
+            chr,
+            pos,
+            ismlog10 = FALSE,
+            chrmargins = 5e6){
+    # chr = locs[,1]; pos = locs[,2]; pvalues = mwas[,3]
+    # z = getMWASandLocations(param)
+    # chr = z$chr; pos = z$start; pvalues = z$`p-value`; 
+    # ismlog10 = FALSE; chrmargins = 0;
 
-# Fast QQ-plot
-# With confidence band and lambda
-# Makes small PDFs even with billions of p-values
-qqPlotFast = function(pvalues, ntests=NULL, ci.level=0.05){
-
-    if(is.null(ntests))
-        ntests = length(pvalues);
-
-    if(is.unsorted(pvalues))
-        pvalues = sort.int(pvalues);
-
-    ypvs = -log10(pvalues);
-    xpvs = -log10(seq_along(ypvs) / ntests);
-
-    if(length(ypvs) > 1000){
-        # need to filter a bit, make the plotting faster
-        levels = as.integer( (xpvs - xpvs[1])/(tail(xpvs,1) - xpvs[1]) * 2000);
-        keep = c(TRUE, diff(levels)!=0);
-        levels = as.integer( (ypvs - ypvs[1])/(tail(ypvs,1) - ypvs[1]) * 2000);
-        keep = keep | c(TRUE, diff(levels)!=0);
-        keep = which(keep);
-        ypvs = ypvs[keep];
-        xpvs = xpvs[keep];
-        #         rm(keep)
-    } else {
-        keep = seq_along(ypvs)
+    stopifnot( length(pvalues) == length(chr) );
+    stopifnot( length(pvalues) == length(pos) );
+        
+    # Factorize chromosome
+    if(is.double(chr))
+        chr = as.integer(chr);
+    if(is.character(chr))
+        chr = factor(chr);
+    if(is.integer(chr)) {
+        levels(chr) = as.character(seq_len(tail(chr,1)));
+        class(chr) = 'factor';
     }
-    mx = head(xpvs,1)*1.05;
-    my = max(mx*1.15,head(ypvs,1))*1.05;
-    plot(NA,NA, ylim = c(0,my), xlim = c(0,mx), xaxs="i", yaxs="i",
-          xlab = expression("- log"[10]*"(p-value), expected under null"),
-          ylab = expression("- log"[10]*"(p-value), observed"));
-    # xlab = "-Log10(p-value), expected under null",
-    # ylab = "-Log10(p-value), observed");
-    lines(c(0,mx),c(0,mx),col="grey")
-    points(xpvs, ypvs, col = "red", pch = 19, cex = 0.25);
 
-    if(!is.null(ci.level)){
-        if((ci.level>0)&(ci.level<1)){
-            quantiles = qbeta(p = rep(c(ci.level/2,1-ci.level/2),
-                                      each=length(xpvs)),
-                              shape1 = keep,
-                              shape2 = ntests - keep + 1);
-            quantiles = matrix(quantiles, ncol=2);
+    # max of each chromosome
+    poslist = split(pos, chr, drop = FALSE);
+    chrmax = sapply(poslist, max) + 0 + chrmargins;
+    
+    # chromosome starts on the plot
+    names(chrmax) = NULL;
+    offsets = c(0, cumsum(chrmax + chrmargins)) + chrmargins;
 
-            lines( xpvs, -log10(quantiles[,1]), col="cyan4")
-            lines( xpvs, -log10(quantiles[,2]), col="cyan4")
+    # within plot coordinates
+    x0 = offsets[unclass(chr)] + pos;
+    if(ismlog10) {
+        y0 = pvalues;
+    } else {
+        y0 = -log10(pvalues);
+    }
+    
+    # Prune the data
+    yfac = as.integer(y0*100)+1L;
+    yorder = sort.list(yfac);
+    levels(yfac) = as.character(seq_len(max(yfac)));
+    class(yfac) = 'factor';
+    
+    ygroup = split(seq_along(yfac), yfac);
+    for( i in seq_along(ygroup)){ # i=1
+        if( length(ygroup[[i]]) > 300 ){
+            ygroup[[i]] = sample(ygroup[[i]], size = 300, replace = FALSE);
         }
     }
-    legend("bottomright",
-           c("P-values", sprintf("%.0f %% Confidence band",100-ci.level*100)),
-           lwd = c(0,1),
-           pch = c(19,NA_integer_),
-           lty = c(0,1),
-           col=c("red","cyan4"))
-    if(length(pvalues)*2>ntests){
-        lambda = sprintf("%.3f",
-                         qchisq(pvalues[ntests/2],1,lower.tail = FALSE) /
-                         qchisq(0.5,1,lower.tail = FALSE));
-        legend("bottom", legend = bquote(lambda == .(lambda)), bty = "n")
-        #         text(mx, mx/2, bquote(lambda == .(lambda)), pos=2)
-    }
-    return(invisible(NULL));
+    # sum(sapply(ygroup, length))
+    keep = unlist(ygroup, use.names = FALSE);
+    
+    # Color code
+    colindex = unclass(chr);
+    
+    # Return minimum object;
+    man = list(
+        x = x0[keep],
+        y = y0[keep],
+        colindex = colindex[keep],
+        offsets = offsets,
+        chrnames = levels(chr)
+    );
+    class(man) = "manPlotInfo";
+    return(man);
 }
+
+manPlotFast = function(
+            man,
+            ylim = NULL,
+            colorSet = c('steelblue4',"#2C82D1","#4CB2D1"),
+            yaxmax = NULL,
+            lwd = 3,
+            axistep = 2,
+            cex = 1){
+    
+    if( class(man) != "manPlotInfo" )
+        stop("The \"man\" parameter is not produced by manPlotPrepare().");
+    
+    # Axis ranges
+    if(is.null(ylim)) {
+        my = max(man$y)*1.05;
+        ylim = c(0,my);
+    } else {
+        my = ylim[2];
+    }
+    if(is.null(yaxmax))
+        yaxmax = floor(my);
+    
+    # Plot frame
+    plot(NA, NA, 
+         xlim = c(0, tail(man$offsets,1)), 
+         ylim = ylim, 
+         xaxs = 'i', 
+         yaxs = 'i',
+         xlab = 'Chromosome', 
+         ylab = expression(paste("\u2013", " log"[10]*"(", italic("P"), "), observed")),
+         # ylab = expression("- log"[10]*"(p-value), observed"),
+         #ylab = "",#expression("- log"[10]*"(p-value)"),
+         axes = FALSE);
+    # abline(v = man$offsets[-c(1,length(man$offsets))], col = 'grey', lwd = 2)
+    axis(side = 1, at = man$offsets, labels = rep("", length(man$offsets)), lwd = lwd);
+    axis(side = 1, 
+         at = (man$offsets[-1] + man$offsets[-length(man$offsets)])/2, 
+         labels = man$chrnames,
+         tick = FALSE,
+         lwd = lwd);
+    axis(2, at = seq(0, yaxmax, axistep), lwd = lwd);
+    
+    # plot points in palette color
+    oldPal = palette(colorSet);
+    points(man$x, man$y, pch = 20, col = ((man$colindex-1L) %% length(colorSet)) + 1L, cex = cex)
+    palette(oldPal);
+}
+
+
