4a745458f294e77f9540b62d78504ab4f185f424
andreyshabalin <andrey.shabalin@gmail.com>
2017-12-20 00:03:47 -0700

    Improved ramwas0createArtificialData(), added parallelization, better performance, use new logging.

--- R/rw0simdata.r
+++ R/rw0simdata.r
@@ -6,116 +6,27 @@ groupSample = function(len, size, gr){
     return(rez);
 }
 
-# Create artificial data set for vignettes and examples
-ramwas0createArtificialData = function(
-            dir,
-            nsamples = 20,
-            nreads = 1e6,
-            ncpgs = 500e3,
-            randseed = 18090212,
-            verbose = TRUE){
+.ramwas0cadJob = function(
+    rng, ld, 
+    locfile, nreads, randseed, fragdistr,
+    age, cpgage1, cpgage2, 
+    sex, cpgsex1, cpgsex2){
+    
+    .log(ld, "%s, Process %06d, Job %02d, Start Generating BAMs %03d-%03d",
+        date(), Sys.getpid(), rng[3], rng[1], rng[2]);
 
-    # dir="D:/temp";nsamples=20;nreads=1e6;
-    # ncpgs=500e3;randseed=18090212;verbose=TRUE
-    set.seed(randseed);
+    locs = readRDS(locfile);
+    locs = locs$chr1;
+    chrlen = tail(locs, 1);
 
-    # Create Directory
-    dir.create(paste0(dir,"/bams"), showWarnings = FALSE, recursive = TRUE);
+    # First 10% CpGs are unmethylated
+    cpgprob = rep(1, length(locs));
+    cpgprob[1:(length(locs)/10)] = 0;
+    
+    for( bam in rng[1]:rng[2] ){ # bam = rng[1]
 
-    # CpG locations
-    {
-        # ncpgs = 500e3
-        chrlen = ncpgs*50;
-        probs = seq(1,0,length.out = chrlen) * (2 * ncpgs / chrlen);
-        locs = which(probs > runif(chrlen)) + 10e3L;
-        # locs[1] = 100L; # for non-CpG count
-        rm(probs);
-
-        cpgset = list( chr1 = locs );
-        saveRDS(
-            object = cpgset,
-            file = paste0(dir, "/Simulated_chromosome.rds"),
-            compress = "xz");
-        if( verbose )
-            message('Simulated CpG locations saved in: ',
-                paste0(dir, "/Simulated_chromosome.rds"));
-    } # locs, cpgset, chrlen, /Single_chromosome.rds
-
-    # Fragment size distribution, with 150 median fragment size
-    {
-        x = 0:250;
-        fragdistr = pmin(1.01*plogis((150-x)/20),1);
-        # plot(x, fragdistr, pch=19);
-        rm(x);
-        if( verbose )
-            message('Generated fragment size distribution');
-    } # fragdistr
-
-    # # Exclude CpGs with density over 15
-    # {
-    #     coverage = ramwas:::calc.coverage(
-    #         rbam = list(startsfwd = cpgset, startsrev = cpgset),
-    #         cpgset = cpgset,
-    #         fragdistr = fragdistr);
-    #     locsgood = locs[ coverage[[1]] <= 15 ];
-    #     rm(coverage, cpgset, locs)
-    # } # locsgood, -locs, -cpgset
-    locsgood = locs;
-
-    # Age covariate and effects
-    {
-        age = sample(x = 20:80, size = nsamples, replace = TRUE)
-        cpgageset = groupSample( 
-                        len = length(locsgood),
-                        size = length(locsgood)/100,
-                        gr = 6);
-        cpgage1 = cpgageset[  1:(length(cpgageset)/2) ];
-        cpgage2 = cpgageset[-(1:(length(cpgageset)/2))];
-        rm(cpgageset)
-    } # age, cpgage1, cpgage2
-
-    # sex
-    {
-        sex = seq_len(nsamples) %% 2L;
-        cpgsexset = groupSample( len = length(locsgood), size = 600, gr = 6);
-        cpgsex1 = cpgsexset[  1:(length(cpgsexset)/2) ];
-        cpgsex2 = cpgsexset[-(1:(length(cpgsexset)/2))];
-        rm(cpgsexset)
-    } # sex, cpgsex1, cpgsex2
-
-    # Covariate file, bam list
-    {
-        cvrt = data.frame(
-            samples = sprintf("BAM%03d", seq_len(nsamples)),
-            age = age,
-            sex = sex,
-            stringsAsFactors = FALSE);
-        write.table(
-                file = paste0(dir,"/covariates.txt"),
-                x = cvrt,
-                sep = "\t",
-                row.names = FALSE);
-        if( verbose )
-            message('Simulated covariates saved in: ',
-                paste0(dir,"/covariates.txt"));
-
-        writeLines(
-            con = paste0(dir,"/bam_list.txt"),
-            text = cvrt$samples);
-        if( verbose )
-            message('List of BAM files saved in: ',
-                paste0(dir,"/bam_list.txt"));
-        
-    } # cvrt, /covariates.txt, /bam_list.txt
-
-    # Main loop
-    # 1/5 of CpGs will be unmethylated
-    cpgprob = rep(1,length(locsgood));
-    cpgprob[1:(length(locsgood)/5)] = 0;
-    for( bam in seq_len(nsamples)){ # bam = 1
-
-        if(verbose)
-            message("Creating BAM ", bam, " of ", nsamples);
+        .log(ld, "%s, Process %06d, Job %02d, BAM: %03d, Generating data",
+            date(), Sys.getpid(), rng[3], bam);
 
         # Change CpG probabilities
         # by age and case-control status
@@ -125,18 +36,21 @@ ramwas0createArtificialData = function(
         cpgprob[cpgsex2] = 1 - sex[bam];
 
         # Pick CpG locations by the probabilities above
+        set.seed(randseed + bam);
         cpglocs = sample(
-                        x = locsgood, 
+                        x = locs,
                         prob = cpgprob,
-                        size = nreads, 
+                        size = nreads,
                         replace = TRUE);
+        # cpglocs = rep(locs, length.out = nreads);
 
         # Add non-CpG reads
-        cpglocs[1:(length(cpglocs)/100)] = seq_len(length(cpglocs)/100);
+        cpglocs[seq_len(length(cpglocs)/100)] = 
+                seq_len(length(cpglocs)/100) + length(fragdistr);
 
         # read strand (0 - forward, 1 - reverse)
         readdir = sample(
-                        x = c(0L,1L), 
+                        x = c(0L, 1L), 
                         size = nreads, 
                         replace = TRUE);
         readlen = sample(
@@ -151,7 +65,7 @@ ramwas0createArtificialData = function(
                         size = nreads, 
                         replace = TRUE);
         # read start (and end) position, as reads are 1bp long
-        readpos = cpglocs - (1L - 2L*readdir)*roffset - readlen * readdir;
+        readpos = cpglocs - (1L - 2L*readdir) * roffset - readlen * readdir;
         rm(roffset, cpglocs);
 
         # Sort reads by location
@@ -161,35 +75,206 @@ ramwas0createArtificialData = function(
         rm(ord);
 
         # Write sam file
-        filesam = sprintf("%s/bams/SAM%03d.sam", dir, bam);
+        .log(ld, "%s, Process %06d, Job %02d, BAM: %03d, Saving SAM",
+            date(), Sys.getpid(), rng[3], bam);
+        filesam = sprintf("%s/SAM%03d.sam", ld, bam);
         fid = file(description = filesam, open = "wt");
         writeLines(
             con = fid,
             sprintf("@HD\tVN:1.0\tSO:unsorted\n@SQ\tSN:chr1\tLN:%d", 
-                chrlen+2e6));
-        writeLines(con = fid,
-            sprintf("%06d\t%d\tchr1\t%d\t%d\t%dM\t*\t0\t0\t*\t*\tNM:i:%d",
-                1:nreads, # 1 QNAME String [!-?A-~]{1,254} Query template NAME
-                readdir*16L, # FLAG Int [0,216-1] bitwise FLAG
+                chrlen + 2e6));
+        
+        step1 = 8192;
+        runto = nreads;
+        nsteps = ceiling(runto/step1);
+        for( part in seq_len(nsteps) ) { # part = 1
+         	fr = (part-1)*step1 + 1;
+         	to = min(part*step1, runto);
+	        set = fr:to;
+	        
+            writeLines(con = fid,
+                sprintf("%06d\t%d\tchr1\t%d\t%d\t%dM\t*\t0\t0\t*\t*\tNM:i:%d",
+                set, # 1 QNAME String [!-?A-~]{1,254} Query template NAME
+                readdir[set]*16L, # FLAG Int [0,216-1] bitwise FLAG
                 # RNAME String \*|[!-()+-<>-~][!-~]* Reference sequence NAME
-                readpos, # POS Int [0,231-1] 1-based leftmost mapping POSition
-                readlen, # mqscore #MAPQ Int [0,2^8-1] MAPping Quality
-                readlen, # 6 CIGAR String  CIGAR string
+                readpos[set], # POS Int [0,231-1] 1-based leftmost mapping POS
+                readlen[set], # mqscore #MAPQ Int [0,2^8-1] MAPping Quality
+                readlen[set], # 6 CIGAR String  CIGAR string
                 # 7 RNEXT String  Ref. name of the mate/next read
                 # 8 PNEXT Int [0,231-1] Position of the mate/next read
                 # 9 TLEN Int [-231+1,231-1] observed Template LENgth
                 # 10 SEQ String \*|[A-Za-z=.]+ segment SEQuence
                 # 11 QUAL String [!-~]+ ASCII of Phred-scaled base QUALity+33
-                75 - readlen));
+                75 - readlen[set]));
+        }
+        rm(part, step1, runto, nsteps, fr, to);
         close(fid);
-        rm(readdir, readpos);
+        rm(readdir, readpos, readlen);
 
+        .log(ld, "%s, Process %06d, Job %02d, BAM: %03d, Convert SAM into BAM",
+            date(), Sys.getpid(), rng[3], bam);
         asBam(
             file = filesam,
-            destination = sprintf("%s/bams/BAM%03d", dir, bam),
+            destination = sprintf("%s/BAM%03d", ld, bam),
             overwrite = TRUE,
             indexDestination = FALSE);
         file.remove(filesam);
+        .log(ld, "%s, Process %06d, Job %02d, BAM: %03d, BAM created",
+            date(), Sys.getpid(), rng[3], bam);
+    }
+    return(invisible(NULL));
+}
+
+# Create artificial data set for vignettes and examples
+ramwas0createArtificialData = function(
+            dir,
+            nsamples = 20,
+            nreads = 1e6,
+            ncpgs = 500e3,
+            randseed = 18090212,
+            threads = 1){
+    
+    ld = paste0(dir, "/bams");
+    dir.create(ld, showWarnings = FALSE, recursive = TRUE);
+    .log(ld, "%s, Start ramwas0createArtificialData() call", date(), 
+        append = FALSE);
+
+    # CpG locations
+    {
+        .log(ld, "%s, Generating %d CpG locations", date(), ncpgs);
+
+        set.seed(randseed);
+        gaps = rnbinom(
+                    n = ncpgs,
+                    size = 1,
+                    prob = seq(1/5, 1/100, length.out = ncpgs));
+        locs = cumsum(2L + gaps) + 1e4L;
+        chrlen = tail(locs, 1);
+        rm(gaps);
+        
+        .log(ld, "%s, Saving CpG locations in: %s", date(), 
+            paste0(dir, "/Simulated_chromosome.rds"));
+        cpgset = list(chr1 = locs);
+        saveRDS(
+            object = cpgset,
+            file = paste0(dir, "/Simulated_chromosome.rds"),
+            compress = "xz");
+        
+    } # locs, cpgset, chrlen, /Single_chromosome.rds
+
+    # Fragment size distribution, with 150 median fragment size
+    {
+        .log(ld, "%s, Generating fragment size distribution", date());
+        x = 0:250;
+        fragdistr = pmin(1.01*plogis((150-x)/20),1);
+        # plot(x, fragdistr, pch=19);
+        rm(x);
+    } # fragdistr
+
+    # Generating covariates
+    {
+        # Age covariate and effects
+        .log(ld, "%s, Generating covariates", date());
+
+        age = sample(x = 20:80, size = nsamples, replace = TRUE)
+        cpgageset = groupSample( 
+                        len = length(locs),
+                        size = length(locs)/100,
+                        gr = 6);
+        cpgage1 = cpgageset[  1:(length(cpgageset)/2) ];
+        cpgage2 = cpgageset[-(1:(length(cpgageset)/2))];
+        rm(cpgageset)
+
+        sex = seq_len(nsamples) %% 2L;
+        cpgsexset = groupSample( 
+                        len = length(locs),
+                        size = 600,
+                        gr = 6);
+        cpgsex1 = cpgsexset[  1:(length(cpgsexset)/2) ];
+        cpgsex2 = cpgsexset[-(1:(length(cpgsexset)/2))];
+        rm(cpgsexset)
+    } # age, cpgage1, cpgage2, sex, cpgsex1, cpgsex2
+
+    # Covariate file, bam list
+    {
+        .log(ld, "%s, Savings covariates in: %s", date(),
+            paste0(dir, "/covariates.txt"));
+        cvrt = data.frame(
+            samples = sprintf("BAM%03d", seq_len(nsamples)),
+            age = age,
+            sex = sex,
+            stringsAsFactors = FALSE);
+        write.table(
+                file = paste0(dir, "/covariates.txt"),
+                x = cvrt,
+                sep = "\t",
+                row.names = FALSE);
+
+        .log(ld, "%s, Savings BAM list in: %s", date(),
+            paste0(dir, "/bam_list.txt"));
+        writeLines(
+            con = paste0(dir, "/bam_list.txt"),
+            text = cvrt$samples);
+    } # cvrt, /covariates.txt, /bam_list.txt
+
+    {
+        .log(ld, "%s, Start generating BAM files in: %s", date(), ld);
+
+        nthreads = min(threads, nsamples);
+        if( nthreads > 1 ){
+            rng = round(seq(1, nsamples+1, length.out = nthreads+1));
+            rangeset = rbind(
+                            rng[-length(rng)],
+                            rng[-1] - 1,
+                            seq_len(nthreads));
+            rangeset = mat2cols(rangeset);
+
+            # library(parallel);
+            cl = makeCluster(nthreads);
+            on.exit({
+                stopCluster(cl);
+            });
+            logfun = .logErrors(ld, .ramwas0cadJob);
+            rez = clusterApplyLB(
+                            cl = cl,
+                            x = rangeset,
+                            fun = logfun,
+                            ld = ld,
+                            locfile = paste0(dir, "/Simulated_chromosome.rds"),
+                            nreads = nreads,
+                            randseed = randseed,
+                            fragdistr = fragdistr,
+                            age = age,
+                            cpgage1 = cpgage1,
+                            cpgage2 = cpgage2,
+                            sex = sex,
+                            cpgsex1 = cpgsex1,
+                            cpgsex2 = cpgsex2);
+            .showErrors(rez);
+            tmp = sys.on.exit();
+            eval(tmp);
+            rm(tmp);
+            on.exit();
+            rm(cl, rng, rangeset, rez);
+            gc();
+        } else {
+            rez = .ramwas0cadJob( 
+                            rng = c(1, nsamples, 0),
+                            ld = ld,
+                            locfile = paste0(dir, "/Simulated_chromosome.rds"),
+                            nreads = nreads,
+                            randseed = randseed,
+                            fragdistr = fragdistr, 
+                            age = age,
+                            cpgage1 = cpgage1,
+                            cpgage2 = cpgage2,
+                            sex = sex,
+                            cpgsex1 = cpgsex1,
+                            cpgsex2 = cpgsex2);
+            if(is.character(rez))
+                stop(rez);
+        }
+        .log(ld, "%s, Done generating BAM files in: %s", date(), ld);
     }
     return(invisible(NULL));
 }
