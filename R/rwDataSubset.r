5b46757f707d3cf91a832dd40046af2a45256b6e
andreyshabalin <andrey.shabalin@gmail.com>
2018-01-06 03:08:19 -0700

    Documented and published subsetCoverageDirByLocation().

--- R/rwDataSubset.r
+++ R/rwDataSubset.r
@@ -1,12 +1,12 @@
 # Subset a coverage matrix and locations set.
-subsetCoverageDirByLocation = function(x, chr, position, targetdir){
+subsetCoverageDirByLocation = function(x, chr, start, targetdir){
     if( is.list(x) ){
         param = parameterPreprocess(x);
         dircov = param$dircoveragenorm;
     } else {
         dircov = x;
     }
-    chrnames = readLines(paste0(dircov,"/CpG_chromosome_names.txt"));
+    chrnames = readLines(paste0(dircov, "/CpG_chromosome_names.txt"));
     if( is.factor(chr) )
         chr = as.character(chr);
     if( is.character(chr) ){
@@ -14,44 +14,51 @@ subsetCoverageDirByLocation = function(x, chr, position, targetdir){
     } else {
         chrn = chr;
     }
-    locmat = fm.load(paste0(dircov,"/CpG_locations"));
-    maxmult = (max(locmat)+2)*2;
+    
+    locs = getLocations(dircov);
+    maxmult = (max(locs$end)+2) * 2;
 
-    mch = match(chrn*maxmult + position, locmat %*% c(maxmult,1), nomatch = 0L);
+    locs$chr = as.integer(locs$chr);
+    mch = match(
+            x = chrn*maxmult + start,
+            table = locs$chr * maxmult + locs$start,
+            nomatch = 0L);
     if( any(mch == 0L) )
         stop("Some locations not found");
 
     mch = sort(mch);
 
     dir.create(targetdir, showWarnings = FALSE)
-    setwd(targetdir)
 
     file.copy(
-            from = paste0(dircov, "/CpG_chromosome_names.txt"),
-            to = "CpG_chromosome_names.txt");
-    fm = fm.create.from.matrix("CpG_locations", locmat[mch,]);
+            from = paste0(dircov,   "/CpG_chromosome_names.txt"),
+            to =   paste0(targetdir, "/CpG_chromosome_names.txt"));
+    
+    fm = fm.create.from.matrix(
+            filenamebase = paste0(targetdir, "/CpG_locations"),
+            mat = as.matrix(locs[mch,]));
     close(fm);
 
-    fi = fm.open(paste0(dircov, "/Coverage"), readonly = TRUE)
+    fi = fm.open(
+                filenamebase = paste0(dircov, "/Coverage"),
+                readonly = TRUE)
     fo = fm.create(
-                filenamebase = "Coverage",
+                filenamebase = paste0(targetdir, "/Coverage"),
                 nrow = nrow(fi),
                 ncol = length(mch),
                 type = fi$type,
                 size = fi$size);
 
-    step1 = 1024;
+    step1 = ceiling( 128*1024*1024 / nrow(fi) / 8);
     runto = length(mch);
     nsteps = ceiling(runto/step1);
     for( part in seq_len(nsteps) ){ # part = 1
         message("Loop filling coverage matrix, step ", part, " of ", nsteps);
         fr = (part-1)*step1 + 1;
         to = min(part*step1, runto);
-        fo[,fr:to] = fi[,mch[fr:to]];
+        fo[, fr:to] = fi[, mch[fr:to]];
     }
     rm(part, step1, runto, nsteps, fr, to);
-
-    # all( fi[,mch] == as.matrix(fo) )
 
     if( !is.null(rownames(fi)))
         rownames(fo) = rownames(fi);
@@ -60,5 +67,6 @@ subsetCoverageDirByLocation = function(x, chr, position, targetdir){
 
     close(fo);
     close(fi);
+    return(invisible(NULL));
 }
 
